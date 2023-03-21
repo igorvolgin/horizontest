d7e0f4cec6a4af9f49f8690949c32490d33cbb3c
igorvolgin <igor.volgin@synapseteam.com>
2023-03-21 02:53:35 +0200

    flush horizon

---
+++ app/Console/Commands/FlushHorizon.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\CreateRecord;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class FlushHorizon extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'horizon:flush';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create n test tasks';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call('queue:flush');
+        Redis::connection()
+            ->del([config('horizon.prefix').'failed:*']);
+        $this->info('each individual failed job flushed');
+        Redis::connection()
+            ->del([config('horizon.prefix').'failed_jobs']);
+        $this->info('failed_jobs flushed');
+    }
+}
